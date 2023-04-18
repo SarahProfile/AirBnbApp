808427009887641b44b88669c09868196dfae414
Sarah Profile <sarahprofile07@gmail.com>
2023-04-18 20:42:11 +0400

    map and seyn

--- src/screens/SearchResultsMap/index.js
+++ src/screens/SearchResultsMap/index.js
@@ -1,24 +1,48 @@
-import { View, Text,useRef, useWindowDimensions } from 'react-native'
-//import React,{ useState } from 'react'
+import { View, Text, useWindowDimensions } from 'react-native'
 import MapView, { Marker,PROVIDER_GOOGLE} from 'react-native-maps'
 import PostCarouseltem from '../../components/PostCarouseltem'
 import places from '../../../assets/data/feed'
 import CustmMarker from '../../components/CustomMarker'
-import { useState } from 'react'
+import { useEffect, useState,useRef } from 'react'
 import { FlatList } from 'react-native-gesture-handler'
 
 
 const SearchResultsMap = (props) => {
-  const { posts } = props;
-  const [selectedPlaceId, setSelectedPlaceId]= useState(null)
-  //const map = useRef();
+  //const { posts } = props;
+  const [selectedPlaceId, setSelectedPlaceId]= useState( null);
+   
+  const map= useRef();
+  const flatlist = useRef();
+  const viewConfig = useRef({itemVisiblePercentThreshold: 70})
+  const onViewChanged = useRef(({viewableItems}) => {
+    if (viewableItems.length > 0) {
+      const selectedPlace = viewableItems[0].item;
+      setSelectedPlaceId(selectedPlace.id)
+    }
+  })
  // const googleMap= useState(PROVIDER_DEFAULT)
  const width = useWindowDimensions().width;
+ useEffect (effect= () => {
+  if(!selectedPlaceId || !flatlist)
+  {
+    return;
+  }
+  const index = places.findIndex(place => place.id === selectedPlaceId)
+  flatlist.current.scrollToIndex({index})
+  const selectedPlace = places[index];
+  const region = {
+    latitude: selectedPlace.coordinate.latitude,
+    longitude : selectedPlace.coordinate.longitude,
+    latitudeDelta: 0.8 ,
+    longitudeDelta: 0.8 ,
+  }
+  map.current.animateToRegion(region);
+ },deps= [selectedPlaceId])
   return (
     <View style={{width: '100%', height: '100%'}}>
        <MapView
-      // ref={map}
-      // provider={PROVIDER_GOOGLE}
+        ref={map}
+      //provider={PROVIDER_GOOGLE}
         style={{width: '100%', height: '100%'}}
        
         initialRegion={{
@@ -40,17 +64,17 @@ const SearchResultsMap = (props) => {
         
       </MapView>
       <View style={{position:'absolute', bottom:10}}>
-        <FlatList
-        data={places}
-        renderItem={({item}) => <PostCarouseltem post={item}
-        shoHorizontalScrollIndicator={false}
-        snapToInterval={width - 60}
-        snapToAlignment={"center"}
-        decelerationRate={"fast"}
-        />
-     
-      }
-      horizontal
+      <FlatList
+          ref={flatlist}
+          data={places}
+          renderItem={({item}) => <PostCarouseltem post={item} />}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          snapToInterval={width - 60}
+          snapToAlignment={"center"}
+          decelerationRate={"fast"}
+          viewabilityConfig={viewConfig.current}
+          onViewableItemsChanged={onViewChanged.current}
         />
    
       </View>
